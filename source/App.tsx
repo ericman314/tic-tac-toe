3ef9a9a15f8af8453c1826fb0d15bf388e12c8f1
Eric Mansfield <ericman314@gmail.com>
2025-03-17 20:35:55 -0600

    Add easy CPU player

--- source/App.tsx
+++ source/App.tsx
@@ -7,9 +7,29 @@ export function App() {
 
 	const [confirmContent, showConfirm] = useConfirmDialog()
 	const [isGameBoardVisible, setIsGameBoardVisible] = React.useState(false)
+	const [numberOfPlayers, setNumberOfPlayers] = React.useState(1)
+	const [cpuPlayer, setCpuPlayer] = React.useState<'X' | 'O'>('X')
 
 	function beginGame() {
 		setIsGameBoardVisible(true)
+	}
+
+	function setNumberOfPlayersAndBeginGame(numberOfPlayers: number) {
+		setNumberOfPlayers(numberOfPlayers)
+		if (numberOfPlayers === 1) {
+			setCpuPlayer(Math.random() > 0.5 ? 'X' : 'O')
+		}
+		beginGame()
+	}
+
+	function promptForNumberOfPlayers() {
+		showConfirm({
+			message: <Text color="blue">How many players?</Text>,
+			okText: '1',
+			cancelText: '2'
+		})
+			.then(() => { setNumberOfPlayersAndBeginGame(1) })
+			.catch(() => { setNumberOfPlayersAndBeginGame(2) })
 	}
 
 	useEffect(() => {
@@ -18,7 +38,7 @@ export function App() {
 			okText: 'Yes!',
 			cancelText: 'No, thank you'
 		})
-			.then(beginGame)
+			.then(promptForNumberOfPlayers)
 			.catch(() => { })
 	}, [])
 
@@ -29,13 +49,13 @@ export function App() {
 			okText: 'Yes',
 			cancelText: 'No'
 		})
-			.then(beginGame)
+			.then(promptForNumberOfPlayers)
 			.catch(() => { })
 	}
 
 	return (
 		<Box>
-			{isGameBoardVisible && <Game onGameOver={handleGameOver} />}
+			{isGameBoardVisible && <Game cpuPlayer={cpuPlayer} onGameOver={handleGameOver} numberOfPlayers={numberOfPlayers} />}
 			{confirmContent}
 		</Box>
 	);
--- source/Game.tsx
+++ source/Game.tsx
@@ -1,15 +1,29 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { Box, Text, useInput } from "ink";
 import { BoardState, getInitialBoardState } from "./BoardState.js";
 import { Board } from "./Board.js";
 
 export type ZeroOneTwo = 0 | 1 | 2
 
-export function Game({ onGameOver }: { onGameOver:()=>void }) {
+type GameProps = {
+  cpuPlayer: 'X' | 'O' | null,
+  numberOfPlayers: number,
+  difficulty: 'easy' | 'intermediate',
+  onGameOver: () => void
+}
+
+export function Game({ cpuPlayer, numberOfPlayers, difficulty = 'easy', onGameOver }: GameProps) {
 
   const [highlightedCell, setHighlightedCell] = React.useState<{ row: ZeroOneTwo, col: ZeroOneTwo }>({ row: 0, col: 0 })
 
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  // Make a first CPU move if the CPU is playing as X
+  useEffect(() => {
+    if (numberOfPlayers === 1 && cpuPlayer === 'X') {
+      dispatch({ type: 'MAKE_CPU_MOVE', difficulty: 'easy' }) // Always make a random move to begin
+    }
+  }, [])
 
   useInput((input, key) => {
 
@@ -35,6 +49,11 @@ export function Game({ onGameOver }: { onGameOver:()=>void }) {
       const { row, col } = highlightedCell
       if (state.board[row][col] === null) {
         dispatch({ type: 'MAKE_PLAYER_MOVE', row, col })
+
+        // After the player goes, make the CPU move. If the game is already finished, this is a no-op.
+        if (numberOfPlayers === 1) {
+          dispatch({ type: 'MAKE_CPU_MOVE', difficulty })
+        }
       }
     }
   })
@@ -97,33 +116,72 @@ type GameState = {
   gameResult: string | null
 }
 
-type Action = {
-  type: 'MAKE_PLAYER_MOVE', row: ZeroOneTwo, col: ZeroOneTwo
-}
+type Action =
+  | { type: 'MAKE_PLAYER_MOVE', row: ZeroOneTwo, col: ZeroOneTwo }
+  | { type: 'MAKE_CPU_MOVE', difficulty: 'easy' | 'intermediate' }
 
 function reducer(state: GameState, action: Action): GameState {
   switch (action.type) {
+
     case 'MAKE_PLAYER_MOVE': {
       const newBoard = cloneBoardState(state.board)
       newBoard[action.row][action.col] = state.currentTurn
-
-      let newGameResult = null
-      if (checkWin(newBoard)) {
-        newGameResult = `The winner is ${state.currentTurn}! Press any key to continue.`
-      }
-      else if (checkDraw(newBoard)) {
-        newGameResult = 'Draw. Press any key to continue.'
-      }
 
       return {
         ...state,
         board: newBoard,
         currentTurn: state.currentTurn === 'X' ? 'O' : 'X',
-        gameResult: newGameResult
+        gameResult: getGameResult({ ...state, board: newBoard })
       }
     }
+
+    case 'MAKE_CPU_MOVE': {
+
+      // Check to make sure we are not already in a draw condition, which would result in an infinite loop
+      if (checkWin(state.board) || checkDraw(state.board)) {
+        return state // no-op
+      }
+
+      switch (action.difficulty) {
+        case 'easy': {
+          // Choose a random empty cell
+          let r, c
+          do {
+            r = Math.floor(Math.random() * 3) as ZeroOneTwo
+            c = Math.floor(Math.random() * 3) as ZeroOneTwo
+          } while (state.board[r][c] !== null)
+
+          const newBoard = cloneBoardState(state.board)
+          newBoard[r][c] = state.currentTurn
+
+          return {
+            ...state,
+            board: newBoard,
+            currentTurn: state.currentTurn === 'X' ? 'O' : 'X',
+            gameResult: getGameResult({ ...state, board: newBoard })
+          }
+        }
+        case 'intermediate': {
+          throw new Error('Not implemented')
+        }
+        default:
+          const _exhaustiveCheck: never = action.difficulty
+          throw new Error(`Unhandled action: ${action}`)
+      }
+    }
+
     default:
-      const _exhaustiveCheck: never = action.type
+      const _exhaustiveCheck: never = action
       throw new Error(`Unhandled action: ${action}`)
   }
+}
+
+function getGameResult(state: GameState): string | null {
+  if (checkWin(state.board)) {
+    return `The winner is ${state.currentTurn}! Press any key to continue.`
+  }
+  else if (checkDraw(state.board)) {
+    return 'Draw. Press any key to continue.'
+  }
+  return null
 }
